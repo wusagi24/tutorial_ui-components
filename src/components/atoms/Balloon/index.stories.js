8a80c83efe02141724ee9accc4a4210c4090929c
nishi wusagi <lapin.wuusan@gmail.com>
2019-01-24 17:38:22 +0900

    配置を定めるという関心を分離

--- src/components/atoms/Balloon/index.stories.js
+++ src/components/atoms/Balloon/index.stories.js
@@ -2,5 +2,16 @@ import React from 'react';
 import Balloon from './index';
 
 export default stories => stories
-  .add('２文字ラベル', () => <Balloon>次へ</Balloon>)
-  .add('４文字ラベル', () => <Balloon>削除する</Balloon>);
+  .add('２文字ラベル',
+    () => <Balloon>次へ</Balloon>)
+  .add('４文字ラベル',
+    () => <Balloon>削除する</Balloon>)
+  .add('絶対座標配置',
+    () => (
+      <Balloon
+        style={{ position: 'absolute', top: '200px', left: '200px' }}
+      >
+        左上から 200px に配置
+      </Balloon>
+    )
+  );
