b98beff5f52c7cb927be61dd33443c8219602424
nishi wusagi <lapin.wuusan@gmail.com>
2019-01-24 17:23:06 +0900

    ラベル内容を表示するという関心の分離

--- src/components/atoms/Balloon/index.stories.js
+++ src/components/atoms/Balloon/index.stories.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import Balloon from './index';
 
-export default stories => stories.add('デフォルト', () => <Balloon />);
+export default stories => stories
+  .add('２文字ラベル', () => <Balloon>次へ</Balloon>)
+  .add('４文字ラベル', () => <Balloon>削除する</Balloon>);
