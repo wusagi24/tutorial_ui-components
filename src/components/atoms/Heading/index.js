3d38ca3c73e210b645f0c2da7acfcf9b812e3bf0
nishi wusagi <lapin.wuusan@gmail.com>
2019-01-24 18:53:20 +0900

    ロジックと表現でコンポーネントを分割

--- src/components/atoms/Heading/index.js
+++ src/components/atoms/Heading/index.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import styles from './style.css';
 
-const Heading = ({
-  children,
+const Heading = props => (
+  <HeadingContainer
+    presenter={ presenterProps => <HeadingPresenter { ...presenterProps } /> }
+    { ...props }
+  />
+);
+
+export const HeadingContainer = ({
+  presenter,
   level = 2,
   visualLevel,
-  className,
   ...props,
 }) => {
-  level = Math.max(0, Math.min(6, level));
+  level = Math.max(1, Math.min(6, level));
   visualLevel = (typeof visualLevel !== 'undefined')
     ? visualLevel
     : level;
-  const Tag = `h${ level }`;
-  const tagStyle = `${ styles.h } ${ styles[`h${ visualLevel }`] }`;
+  const tag = `h${ level }`;
 
-  return (
-    <Tag
-      className={[ tagStyle, className ].join(' ')}
-      { ...props }
-    >
-      { children }
-    </Tag>
-  );
+  return presenter({ tag, visualLevel, ...props });
 };
+
+export const HeadingPresenter = ({
+  tag: Tag,
+  visualLevel,
+  className,
+  ...props,
+}) => (
+  <Tag
+    className={[ styles.h, styles[`h${ visualLevel }`], className ].join(' ')}
+    { ...props }
+  />
+);
 
 export default Heading;
