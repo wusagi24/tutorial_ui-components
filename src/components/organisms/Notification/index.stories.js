38a9ac5eca2c62654ed4aeb41502876fdb731e70
nishi wusagi <lapin.wuusan@gmail.com>
2019-02-07 19:20:51 +0900

    Notification コンポーネントの実装

---
+++ src/components/organisms/Notification/index.stories.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import Notification from './index';
+
+const notification = {
+  id: 0,
+  thumbnail: '/mock/images/img01.jpg',
+  title: 'コンポーネント指向で UI を設計しよう！第１話',
+  channelName: 'UI チャンネル',
+  startAt: 1507032000000,
+  endAt: 1507035600000,
+};
+
+export default stories => stories
+  .add(
+    'デフォルト',
+    () => (
+      <Notification program={ notification } onClickDelete={ action('削除ボタンがクリックされました') } />
+    ),
+  );
