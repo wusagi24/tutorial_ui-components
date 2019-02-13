1bad4b02aa50d074d52852b0bb5e730568e127d1
nishi wusagi <lapin.wuusan@gmail.com>
2019-02-13 16:08:33 +0900

    BalloonTip コンポーネントの実装
    任意の要素に対してバルーンチップを表示させられるコンポーネントの実装

--- src/components/atoms/Balloon/index.js
+++ src/components/atoms/Balloon/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+
 import styles from './styles.css';
+
+import HoverTipInteraction, { Tip, Marker } from '../HoverTipInteraction/index';
 
 const Balloon = ({ children, className, ...props }) => (
   <span className={ [ styles.balloon, className ].join(' ') } { ...props }>
 { children }</span>
+);
+
+export const BalloonTip = ({ children, label, className, ...props }) => (
+  <HoverTipInteraction
+    className={ className }
+    { ...props }
+  >
+    <Marker><span>{ children }</span></Marker>
+    <Tip><Balloon>{ label }</Balloon></Tip>
+  </HoverTipInteraction>
 );
 
 export default Balloon;
--- src/components/atoms/Balloon/index.stories.js
+++ src/components/atoms/Balloon/index.stories.js
@@ -7,9 +7,18 @@ import HoverTipInteraction, { Tip } from '../HoverTipInteraction/index.js';
 */
 
 import React from 'react';
-import Balloon from './index.js';
+
+import Balloon, { BalloonTip } from './index.js';
+import { TrashCanIcon } from '../Icon/index.js';
+
+import { withStyle } from '../../utils/decorators';
 
 export default stories => stories
   .add('2文字ラベル', () => <Balloon>次へ</Balloon>)
   .add('4文字ラベル', () => <Balloon>削除する</Balloon>)
-  .add('絶対座標配置', () => <Balloon style={{ position: 'absolute', top: '200px', left: '200px' }}>左上から 200px に配置</Balloon>);
+  .add('絶対座標配置', () => <Balloon style={{ position: 'absolute', top: '200px', left: '200px' }}>左上から 200px に配置</Balloon>)
+  .add('アイコンラベル', () => <Balloon><TrashCanIcon /></Balloon>)
+  .add('絵文字', () => <Balloon>✖</Balloon>)
+  .add('バルーンチップ', () => withStyle({ marginTop: '50px' })(
+    <p>ここに<BalloonTip label="注釈を記述するUI">バルーンチップ</BalloonTip>を表示</p>
+  ));
